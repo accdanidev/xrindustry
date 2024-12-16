bc56489aa0fb2de55a1b2dbdb5a4e77293c9cd01
daniel vilchis <daniel.vilchis@accenture.com>
2024-12-15 23:05:11 -0600

    window movcement

--- Assets/Scripts/ControllerRayCast.cs
+++ Assets/Scripts/ControllerRayCast.cs
@@ -2,18 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Fusion.XR.Shared.Locomotion;
+#if ENABLE_INPUT_SYSTEM
+using UnityEngine.InputSystem;
+#endif
 
 public class ControllerRaycast : MonoBehaviour
 {
-    [SerializeField] Vector3 firstRayHit, secondRayHit, initialScale;
-    [SerializeField] Vector3 lastPosition;
-    [SerializeField] float currentDistance, initialDistance;
+    //[SerializeField] Vector3 firstRayHit, secondRayHit, initialScale;
+    //[SerializeField] Vector3 lastPosition;
+    //[SerializeField] float currentDistance, initialDistance;
+    [SerializeField] Transform rayOrigin;
     [SerializeField] GameObject selectedObject;
-    [SerializeField] bool hitted = false;
+    [SerializeField] bool hitted = false, selected = false;
+   // [SerializeField] LineRenderer lineRenderer;
+    private Vector3 hitOffset;
 
-    RayBeamer ray;
-
-    [SerializeField] Camera mainCamera; // Cámara que emitirá el rayo
 
     void Start()
     {
@@ -22,26 +25,61 @@ public class ControllerRaycast : MonoBehaviour
 
     void Update()
     {
-        if (hitted && selectedObject != null)
-        {
-            print(ray.lastHit);
-        }
-        else
+        if (selected)
         {
 
-        }
+            // Renderizar el rayo desde el origen
+            //lineRenderer.SetPosition(0, rayOrigin.position);
 
+            RaycastHit hit;
+            if (Physics.Raycast(rayOrigin.position, rayOrigin.forward, out hit, 2000))
+            {
+               // lineRenderer.SetPosition(1, hit.point);
+
+                // Detectar el objeto impactado
+                if (hit.collider.CompareTag("Grabber"))
+                {
+                    if (!hitted) // Solo selecciona si no hay un objeto ya seleccionado
+                    {
+                        selectedObject = hit.collider.gameObject;
+                        hitted = true;
+
+                        // Calcular el desplazamiento inicial entre el objeto y el punto de impacto
+                        hitOffset = selectedObject.transform.position - hit.point;
+                    }
+
+                    // Mover el objeto al seguir impactándolo
+                    if (selectedObject != null)
+                    {
+                        selectedObject.transform.position = hit.point + hitOffset;
+                    }
+                }
+            }
+            else
+            {
+                // Si no hay impacto, extender el rayo hacia adelante
+                //lineRenderer.SetPosition(1, rayOrigin.position + rayOrigin.forward * 2000);
+
+                // Soltar el objeto si no se está impactando
+                if (hitted)
+                {
+                    hitted = false;
+                    selectedObject = null;
+                }
+            }
+
+        }
     }
 
 
-    public void Hitted(RayBeamer rayB)
+    public void Hitted()
     {
-        ray = rayB;
-        hitted = true;
+        selected = true;
     }
 
     public void HitOut()
     {
-
+        selected = false;
+        hitted = false;
     }
 }
